d874900b2e72c6ea9e4f843bd899f03077e9ff3c
YudhaPew <yudhaprasetya678@gmail.com>
2023-10-07 00:17:27 +0700

    Tugas Prakweb .2

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "prakweb2023b_213040077") or die('Connections_failed');
+$conn = mysqli_connect("localhost", "root", "", "prakweb_2023_b_213040077") or die('Connections_failed');
 
 $buku = query("SELECT * FROM buku");
 
@@ -21,7 +21,7 @@ function query($query)
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Bootstrap demo</title>
+	<title>TUGAS PW</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
